0adcd529136d10f64ed8e059168659021aa67234
Vladislav Klopota <vladislav997@yahoo.com>
2023-03-10 22:46:51 +0300

    feature: сonfiguration .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+      cache: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
