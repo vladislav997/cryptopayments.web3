d1bd53220f3f956e146effe8e86add04feab67d4
Vladislav Klopota <vladislav997@yahoo.com>
2023-03-11 12:20:14 +0300

    feature: balance, send & transactions methods for trc, DTOs, validation

--- src/web3/trc/v1/trc.controller.ts
+++ src/web3/trc/v1/trc.controller.ts
@@ -1,5 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
 import { TrcServiceV1 } from './trc.service';
+import { BalanceTrcDto } from './dto/balance-trc.dto';
+import { SendTrcDto } from './dto/send-trc.dto';
+import { TransactionsTrcDto } from './dto/transactions-trc-dto';
 
 @Controller({
   path: 'trc',
@@ -8,8 +11,23 @@ import { TrcServiceV1 } from './trc.service';
 export class TrcControllerV1 {
   constructor(private readonly trcService: TrcServiceV1) {}
 
-  @Get('/create')
+  @Post('/create')
   create() {
     return this.trcService.create();
+  }
+
+  @Post('/balance')
+  balance(@Body(new ValidationPipe()) balanceTrcDto: BalanceTrcDto) {
+    return this.trcService.balance(balanceTrcDto);
+  }
+
+  @Post('/send')
+  send(@Body(new ValidationPipe()) sendTrcDto: SendTrcDto) {
+    return this.trcService.send(sendTrcDto);
+  }
+
+  @Post('/transactions')
+  transactions(@Body(new ValidationPipe()) transactionsTrcDto: TransactionsTrcDto) {
+    return this.trcService.transactions(transactionsTrcDto);
   }
 }
--- src/web3/trc/v1/trc.service.ts
+++ src/web3/trc/v1/trc.service.ts
@@ -1,6 +1,7 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import * as TronWeb from 'tronweb';
 import { HttpService } from '@nestjs/axios';
+import { AxiosResponse } from 'axios';
+import * as TronWeb from 'tronweb';
 
 @Injectable()
 export class TrcServiceV1 {
@@ -27,6 +28,108 @@ export class TrcServiceV1 {
           privateKey: response.privateKey,
           publicKey: response.publicKey,
         },
+      };
+    } catch (e) {
+      throw new HttpException(
+        {
+          status: false,
+          message: e.message,
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        {
+          cause: e,
+        },
+      );
+    }
+  }
+
+  async balance(balanceTrcDto) {
+    try {
+      const tronWeb = this.tronWebCall();
+      tronWeb.setAddress(balanceTrcDto.address);
+      const contract = await tronWeb.contract().at(balanceTrcDto.contract);
+
+      const response = await contract.balanceOf(balanceTrcDto.address).call();
+      const decimal = await contract.decimals().call();
+      const getDecimal = Math.pow(10, decimal);
+
+      const balance = tronWeb.BigNumber(response._hex) / getDecimal;
+
+      return {
+        status: true,
+        data: balance,
+      };
+    } catch (e) {
+      throw new HttpException(
+        {
+          status: false,
+          message: e.message,
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        {
+          cause: e,
+        },
+      );
+    }
+  }
+
+  async send(sendTrcDto) {
+    let contract;
+    let amount;
+
+    try {
+      const tronWeb = this.tronWebCall();
+      tronWeb.setPrivateKey(sendTrcDto.private_key);
+      contract = await tronWeb.contract().at(sendTrcDto.contract);
+      const decimalValue = await contract.decimals().call();
+      const getDecimal = Math.pow(10, decimalValue);
+      amount = parseInt(String(Number(sendTrcDto.amount) * getDecimal));
+
+      const transaction = await contract
+        .transfer(sendTrcDto.to_address, BigInt(amount.toString()))
+        .send();
+
+      if (transaction) {
+        return {
+          status: true,
+          data: {
+            hash: transaction,
+          },
+        };
+      } else {
+        return {
+          status: false,
+          message: 'Transaction failed',
+        };
+      }
+    } catch (e) {
+      throw new HttpException(
+        {
+          status: false,
+          message: e.message,
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        {
+          cause: e,
+        },
+      );
+    }
+  }
+
+  async transactions(transactionsTrcDto) {
+    try {
+      const url =
+        'https://api.trongrid.io/v1/accounts/' +
+        transactionsTrcDto.address +
+        '/transactions/trc20?&contract_address=' +
+        transactionsTrcDto.contract;
+      const response: AxiosResponse<Response[]> = await this.httpService
+        .get(url)
+        .toPromise();
+
+      return {
+        status: true,
+        data: response.data['data'],
       };
     } catch (e) {
       throw new HttpException(
