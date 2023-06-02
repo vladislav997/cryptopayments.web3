bf5cf159a1f71f8b5f95f5b3011b8d16da62a434
Vladislav Klopota <vladislav997@yahoo.com>
2023-06-02 23:40:01 +0300

    refactoring: Web3 Service

--- src/web3/web3.service.ts
+++ src/web3/web3.service.ts
@@ -1,56 +1,83 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { getAddressByPrivateKeyWeb3 } from '../common/helper/helper.function';
-import { AxiosResponse } from 'axios';
-import { HttpService } from '@nestjs/axios';
+import axios from 'axios';
 import contractJson from '../common/helper/contract-json';
 const Web3 = require('web3');
 
 @Injectable()
 export class Web3Service {
-  constructor(private readonly httpService: HttpService) {}
+  private apiEtherscanUrl = 'https://api.etherscan.io/api';
+  private apiBscscanUrl = 'https://api.bscscan.com/api';
 
   async getContractDecimal(web3, contract) {
-    const getContract = new web3.eth.Contract(contractJson(), contract);
-    return await getContract.methods.decimals().call();
+    try {
+      const getContract = new web3.eth.Contract(contractJson(), contract);
+      // вызов функции decimals контракта для получения количества десятичных знаков
+      return await getContract.methods.decimals().call();
+    } catch (e) {
+      throw new HttpException(
+        {
+          message: e.message,
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        {
+          cause: e,
+        },
+      );
+    }
   }
 
   async balanceCall(web3, contract, address) {
-    const getContract = new web3.eth.Contract(contractJson(), contract);
-    return await getContract.methods.balanceOf(address).call();
+    try {
+      const getContract = new web3.eth.Contract(contractJson(), contract);
+      // вызов функции balanceOf контракта для получения баланса
+      return await getContract.methods.balanceOf(address).call();
+    } catch (e) {
+      throw new HttpException(
+        {
+          message: e.message,
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        {
+          cause: e,
+        },
+      );
+    }
   }
 
   async calculateEstimateGasFees(web3, amount, address, dto) {
-    let usedGasLimit = dto.gas_limit;
-    let estimateGas;
     const gasPrice = await web3.eth.getGasPrice();
-    let gasFees = Number(usedGasLimit) * Number(gasPrice);
+    let usedGasLimit = dto.gas_limit;
+    let estimateGas = 0;
+    let gasFees = 0;
     const contract = new web3.eth.Contract(contractJson(), dto.contract);
 
-    if (Number(usedGasLimit) > 0) {
-      gasFees = Number(usedGasLimit) * Number(gasPrice);
-    } else {
+    if (Number(usedGasLimit) <= 0) {
+      // оценка необходимого лимита газа для выполнения транзакции
       estimateGas = await contract.methods
         .transfer(dto.to_address, amount)
         .estimateGas({ from: address });
+      // рассчет использованного предела газа
       usedGasLimit = parseInt(String(estimateGas / 2)) + estimateGas;
     }
 
+    // вычисление общих комиссий в газе
+    gasFees = Number(usedGasLimit) * Number(gasPrice);
+
+    // преобразование комиссий из Wei в Ether
     const finalGasFees = Web3.utils.fromWei(gasFees.toString());
 
     return {
-      amount: amount,
-      tx: 'ok',
       gasLimit: usedGasLimit,
-      gasPrice: gasPrice,
+      gasPrice,
       estimateGasFees: finalGasFees,
     };
   }
 
   async create(chainLink) {
     try {
-      const connectWeb3 = new Web3(new Web3.providers.HttpProvider(chainLink));
-
-      const wallet = await connectWeb3.eth.accounts.create();
+      const web3 = new Web3(new Web3.providers.HttpProvider(chainLink));
+      const wallet = await web3.eth.accounts.create();
 
       return {
         status: true,
@@ -71,53 +98,57 @@ export class Web3Service {
 
   async balance(chainLink, balanceWeb3Dto) {
     try {
-      let balance;
       const web3 = new Web3(chainLink);
-      const validateAddress = new Web3().utils.isAddress(
-        balanceWeb3Dto.address,
-      );
+      const isAddressValid = web3.utils.isAddress(balanceWeb3Dto.address);
 
-      if (validateAddress) {
-        if (balanceWeb3Dto.type == 'coin') {
-          const getBalance = await web3.eth.getBalance(balanceWeb3Dto.address);
-
-          balance = parseFloat(
-            web3.utils.fromWei(getBalance.toString(), 'ether'),
-          );
-        }
-
-        if (balanceWeb3Dto.type == 'token') {
-          const validateContract = new Web3().utils.isAddress(
-            balanceWeb3Dto.contract,
-          );
-
-          if (validateContract) {
-            const decimal = await this.getContractDecimal(
-              web3,
-              balanceWeb3Dto.contract,
-            );
-            const balanceCall = await this.balanceCall(
-              web3,
-              balanceWeb3Dto.contract,
-              balanceWeb3Dto.address,
-            );
-
-            balance = balanceCall / Math.pow(10, decimal);
-          } else {
-            throw new HttpException(
-              'Incorrect contract address',
-              HttpStatus.BAD_REQUEST,
-            );
-          }
-        }
-
-        return {
-          status: true,
-          data: balance,
-        };
-      } else {
+      if (!isAddressValid) {
         throw new HttpException('Incorrect address', HttpStatus.BAD_REQUEST);
       }
+
+      let balance;
+
+      if (balanceWeb3Dto.type == 'coin') {
+        // получение баланса Эфира
+        const getBalance = await web3.eth.getBalance(balanceWeb3Dto.address);
+
+        // преобразование баланса из Wei в Ether
+        balance = parseFloat(
+          web3.utils.fromWei(getBalance.toString(), 'ether'),
+        );
+      }
+
+      if (balanceWeb3Dto.type == 'token') {
+        const isContractValid = web3.utils.isAddress(balanceWeb3Dto.contract);
+
+        // проверка валидности адреса контракта
+        if (!isContractValid) {
+          throw new HttpException(
+            'Incorrect contract address',
+            HttpStatus.BAD_REQUEST,
+          );
+        }
+
+        // получение decimal для токена
+        const decimal = await this.getContractDecimal(
+          web3,
+          balanceWeb3Dto.contract,
+        );
+
+        // вызов функции balanceOf контракта токена
+        const balanceCall = await this.balanceCall(
+          web3,
+          balanceWeb3Dto.contract,
+          balanceWeb3Dto.address,
+        );
+
+        // преобразование баланса токена
+        balance = balanceCall / Math.pow(10, decimal);
+      }
+
+      return {
+        status: true,
+        data: balance,
+      };
     } catch (e) {
       throw new HttpException(
         {
@@ -133,105 +164,43 @@ export class Web3Service {
 
   async send(chainLink, chainId, sendWeb3Dto) {
     try {
-      let transaction;
-      const web3 = new Web3(chainLink);
-      const address = getAddressByPrivateKeyWeb3(web3, sendWeb3Dto.private_key);
-      const validateRecipientAddress = new Web3().utils.isAddress(
+      // проверка валидности адреса получателя
+      const isRecipientAddressValid = new Web3.utils.isAddress(
         sendWeb3Dto.to_address,
       );
 
-      if (validateRecipientAddress) {
-        if (sendWeb3Dto.type == 'coin') {
-          const amount = Web3.utils.toWei(
-            sendWeb3Dto.amount.toString(),
-            'ether',
-          );
-          const nonce = await web3.eth.getTransactionCount(address, 'latest');
-          const usedGasLimit =
-            sendWeb3Dto.gas_limit > 0 ? sendWeb3Dto.gas_limit : 63000;
-
-          transaction = {
-            from: address,
-            nonce: web3.utils.toHex(nonce),
-            gas: usedGasLimit,
-            to: sendWeb3Dto.to_address,
-            value: amount,
-            chainId: chainId,
-          };
-        }
-
-        if (sendWeb3Dto.type == 'token') {
-          const validateContract = new Web3().utils.isAddress(
-            sendWeb3Dto.contract,
-          );
-          if (validateContract) {
-            const decimal = await this.getContractDecimal(
-              web3,
-              sendWeb3Dto.contract,
-            );
-
-            const amount = (
-              sendWeb3Dto.amount * Math.pow(10, decimal)
-            ).toLocaleString('fullwide', { useGrouping: false });
-
-            const calculateEstimateGasFees =
-              await this.calculateEstimateGasFees(
-                web3,
-                amount,
-                address,
-                sendWeb3Dto,
-              );
-            const contract = new web3.eth.Contract(
-              contractJson(),
-              sendWeb3Dto.contract,
-            );
-
-            transaction = {
-              from: address,
-              to: sendWeb3Dto.contract,
-              gas: calculateEstimateGasFees.gasLimit,
-              data: contract.methods
-                .transfer(sendWeb3Dto.to_address, amount)
-                .encodeABI(),
-            };
-          } else {
-            throw new HttpException(
-              'Incorrect contract address',
-              HttpStatus.BAD_REQUEST,
-            );
-          }
-        }
-
-        const signTransaction = await web3.eth.accounts.signTransaction(
-          transaction,
-          sendWeb3Dto.private_key,
-        );
-        const receipt = await new Promise<any>((resolve, reject) =>
-          web3.eth
-            .sendSignedTransaction(signTransaction.rawTransaction)
-            .on('receipt', resolve)
-            .on('error', reject),
-        );
-
-        const response = {
-          is_success_transaction: receipt.status,
-          transaction_id: receipt.transactionHash,
-          from: address,
-          to: sendWeb3Dto.to_address,
-          value: String(sendWeb3Dto.amount),
-          timestamp: String(Date.now()),
-        };
-
-        return {
-          status: true,
-          data: response,
-        };
-      } else {
+      if (!isRecipientAddressValid) {
         throw new HttpException(
           'Incorrect recipient address',
           HttpStatus.BAD_REQUEST,
         );
       }
+
+      let transaction;
+      const web3 = new Web3(chainLink);
+      const address = getAddressByPrivateKeyWeb3(web3, sendWeb3Dto.private_key);
+
+      // определение типа отправки: Эфир или токен
+      if (sendWeb3Dto.type == 'coin') {
+        // формирование объекта транзакции для отправки эфира
+        transaction = await this.coinSend(web3, chainId, sendWeb3Dto, address);
+      } else if (sendWeb3Dto.type == 'token') {
+        // формирование объекта транзакции для отправки токена
+        transaction = await this.tokenSend(web3, sendWeb3Dto, address);
+      }
+
+      // подпись и отправка транзакции
+      const response = await this.signedAndSendTransaction(
+        web3,
+        sendWeb3Dto,
+        transaction,
+        address,
+      );
+
+      return {
+        status: true,
+        data: response,
+      };
     } catch (e) {
       throw new HttpException(
         {
@@ -245,103 +214,207 @@ export class Web3Service {
     }
   }
 
+  async coinSend(web3, chainId, sendWeb3Dto, address) {
+    try {
+      // преобразование суммы в единицы wei
+      const amount = Web3.utils.toWei(sendWeb3Dto.amount.toString(), 'ether');
+
+      // получение текущего nonce для адреса
+      const nonce = await web3.eth.getTransactionCount(address, 'latest');
+
+      // определение используемого лимита газа
+      const usedGasLimit =
+        sendWeb3Dto.gas_limit > 0 ? sendWeb3Dto.gas_limit : 63000;
+
+      // формирование объекта транзакции для отправки эфира
+      return {
+        from: address,
+        nonce: web3.utils.toHex(nonce),
+        gas: usedGasLimit,
+        to: sendWeb3Dto.to_address,
+        value: amount,
+        chainId: chainId,
+      };
+    } catch (e) {
+      throw new HttpException(
+        {
+          message: e.message,
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        {
+          cause: e,
+        },
+      );
+    }
+  }
+
+  async tokenSend(web3, sendWeb3Dto, address) {
+    try {
+      // проверка валидности адреса контракта
+      const isValidateContract = new Web3().utils.isAddress(
+        sendWeb3Dto.contract,
+      );
+      if (!isValidateContract) {
+        throw new HttpException(
+          'Incorrect contract address',
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+
+      // получение количества decimal у контракта
+      const decimal = await this.getContractDecimal(web3, sendWeb3Dto.contract);
+
+      // расчет суммы перевода в соответствии с decimal
+      const amount = (
+        sendWeb3Dto.amount * Math.pow(10, decimal)
+      ).toLocaleString('fullwide', { useGrouping: false });
+
+      // расчет оценочной стоимости газа для транзакции
+      const calculateEstimateGasFees = await this.calculateEstimateGasFees(
+        web3,
+        amount,
+        address,
+        sendWeb3Dto,
+      );
+      const contract = new web3.eth.Contract(
+        contractJson(),
+        sendWeb3Dto.contract,
+      );
+
+      // формирование объекта транзакции для отправки токенов
+      return {
+        from: address,
+        to: sendWeb3Dto.contract,
+        gas: calculateEstimateGasFees.gasLimit,
+        data: contract.methods
+          .transfer(sendWeb3Dto.to_address, amount)
+          .encodeABI(),
+      };
+    } catch (e) {
+      throw new HttpException(
+        {
+          message: e.message,
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        {
+          cause: e,
+        },
+      );
+    }
+  }
+
+  async signedAndSendTransaction(web3, sendWeb3Dto, transaction, address) {
+    // подпись транзакции с использованием приватного ключа и отправкой
+    const signTransaction = await web3.eth.accounts.signTransaction(
+      transaction,
+      sendWeb3Dto.private_key,
+    );
+    // ожидание получения результата выполнения транзакции
+    const receipt = await new Promise<any>((resolve, reject) =>
+      web3.eth
+        .sendSignedTransaction(signTransaction.rawTransaction)
+        .on('receipt', resolve)
+        .on('error', reject),
+    );
+
+    // формирование объекта с результатами выполнения транзакции
+    return {
+      is_success_transaction: receipt.status,
+      transaction_id: receipt.transactionHash,
+      from: address,
+      to: sendWeb3Dto.to_address,
+      value: String(sendWeb3Dto.amount),
+      timestamp: String(Date.now()),
+    };
+  }
+
   async transactions(chainLink, chainId, apiKey, transactionsWeb3Dto) {
     try {
-      const validateAddress = new Web3().utils.isAddress(
+      const isAddressValid = new Web3().utils.isAddress(
         transactionsWeb3Dto.address,
       );
 
-      if (validateAddress) {
-        let result;
-        let url;
-
-        switch (chainId) {
-          case '1':
-            url = 'https://api.etherscan.io/api?module=account';
-            break;
-          case '56':
-            url = 'https://api.bscscan.com/api?module=account';
-            break;
-        }
-
-        if (transactionsWeb3Dto.type == 'coin') {
-          url = url + '&action=txlist' + '&endblock=99999999';
-        }
-
-        if (transactionsWeb3Dto.type == 'token') {
-          const validateContract = new Web3().utils.isAddress(
-            transactionsWeb3Dto.contract,
-          );
-          if (validateContract) {
-            url =
-              url +
-              '&action=tokentx' +
-              '&endblock=999999999' +
-              '&contractaddress=' +
-              transactionsWeb3Dto.contract;
-          } else {
-            throw new HttpException(
-              'Incorrect contract address',
-              HttpStatus.BAD_REQUEST,
-            );
-          }
-        }
-
-        url =
-          url +
-          '&address=' +
-          transactionsWeb3Dto.address +
-          '&startblock=0' +
-          '&sort=asc' +
-          '&apikey=' +
-          apiKey;
-
-        const response: AxiosResponse<Response[]> = await this.httpService
-          .get(url)
-          .toPromise();
-
-        if (transactionsWeb3Dto.type == 'coin') {
-          result = response.data['result'].map(
-            ({ txreceipt_status, hash, from, to, value, timeStamp }) => ({
-              is_success_transaction: txreceipt_status == 1,
-              transaction_id: hash,
-              from: from,
-              to: to,
-              value: Web3.utils.fromWei(value.toString(), 'ether'),
-              timestamp: timeStamp,
-            }),
-          );
-        }
-
-        if (transactionsWeb3Dto.type == 'token') {
-          const web3 = new Web3(chainLink);
-          const decimal = await this.getContractDecimal(
-            web3,
-            transactionsWeb3Dto.contract,
-          );
-
-          result = response.data['result'].map(
-            ({ tokenSymbol, hash, from, to, value, timeStamp }) => ({
-              token_symbol: tokenSymbol,
-              transaction_id: hash,
-              from: from,
-              to: to,
-              value: (value / Math.pow(10, decimal)).toLocaleString(
-                'fullwide',
-                { useGrouping: false },
-              ),
-              timestamp: timeStamp,
-            }),
-          );
-        }
-
-        return {
-          status: true,
-          data: result,
-        };
-      } else {
+      if (!isAddressValid) {
         throw new HttpException('Incorrect address', HttpStatus.BAD_REQUEST);
       }
+
+      let result;
+      let url;
+
+      // формирование API URL в зависимости от выбранной сети
+      switch (chainId) {
+        case '1':
+          url = `${this.apiEtherscanUrl}?module=account`;
+          break;
+        case '56':
+          url = `${this.apiBscscanUrl}?module=account`;
+          break;
+      }
+
+      if (transactionsWeb3Dto.type == 'coin') {
+        url += '&action=txlist&endblock=99999999';
+      }
+
+      if (transactionsWeb3Dto.type == 'token') {
+        // проверка валидности адреса контракта
+        const isContractValid = new Web3().utils.isAddress(
+          transactionsWeb3Dto.contract,
+        );
+
+        if (!isContractValid) {
+          throw new HttpException(
+            'Incorrect contract address',
+            HttpStatus.BAD_REQUEST,
+          );
+        }
+
+        url += `&action=tokentx&endblock=999999999&contractaddress=${transactionsWeb3Dto.contract}`;
+      }
+
+      url += `&address=${transactionsWeb3Dto.address}&startblock=0&sort=asc&apikey=${apiKey}`;
+
+      const response = await axios.get(url);
+
+      if (transactionsWeb3Dto.type == 'coin') {
+        // обработка результатов для типа 'coin'
+        result = response.data.result.map(
+          ({ txreceipt_status, hash, from, to, value, timeStamp }) => ({
+            is_success_transaction: txreceipt_status == 1,
+            transaction_id: hash,
+            from,
+            to,
+            value: Web3.utils.fromWei(value.toString(), 'ether'),
+            timestamp: timeStamp,
+          }),
+        );
+      }
+
+      if (transactionsWeb3Dto.type == 'token') {
+        // обработка результатов для типа 'token'
+        const web3 = new Web3(chainLink);
+        const decimal = await this.getContractDecimal(
+          web3,
+          transactionsWeb3Dto.contract,
+        );
+
+        result = response.data.result.map(
+          ({ tokenSymbol, hash, from, to, value, timeStamp }) => ({
+            token_symbol: tokenSymbol,
+            transaction_id: hash,
+            from,
+            to,
+            value: (value / Math.pow(10, decimal)).toLocaleString('fullwide', {
+              useGrouping: false,
+            }),
+            timestamp: timeStamp,
+          }),
+        );
+      }
+
+      return {
+        status: true,
+        data: result,
+      };
     } catch (e) {
       throw new HttpException(
         {
