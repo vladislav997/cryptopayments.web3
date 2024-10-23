23526cf12d7c5cdb7d253d3ceaaa383490756976
Vladislav Klopota <vladislav997@yahoo.com>
2024-10-23 09:44:18 +0200

    feature: separate logs by day

--- src/common/configs/logger.config.ts
+++ src/common/configs/logger.config.ts
@@ -1,27 +1,41 @@
 import { createLogger, transports, format } from 'winston';
+import * as DailyRotateFile from 'winston-daily-rotate-file';
 
 const loggerOptions = {
+  level: 'info',
   transports: [
     new transports.Console(),
-    new transports.File({
-      filename: 'logs/error.log',
+    new DailyRotateFile({
+      dirname: 'logs',
+      filename: 'backend-error-%DATE%.log',
+      datePattern: 'DD.MM.YYYY',
       level: 'error',
-      format: format.json(),
+      format: format.combine(
+        format.timestamp({
+          format: process.env.DATETIME_LOGGER_FORMAT || 'DD.MM.YYYY HH:mm:ss',
+        }),
+        format.json(),
+        format.printf(
+          (info) => `${info.timestamp} ${info.level}: ${info.message}`,
+        ),
+      ),
     }),
-    new transports.File({
-      filename: 'logs/combined.log',
-      format: format.json(),
+    new DailyRotateFile({
+      dirname: 'logs',
+      filename: 'backend-info-%DATE%.log',
+      datePattern: 'DD.MM.YYYY',
+      level: 'info',
+      format: format.combine(
+        format.timestamp({
+          format: process.env.DATETIME_LOGGER_FORMAT || 'DD.MM.YYYY HH:mm:ss',
+        }),
+        format.json(),
+        format.printf(
+          (info) => `${info.timestamp} ${info.level}: ${info.message}`,
+        ),
+      ),
     }),
   ],
-  format: format.combine(
-    format.timestamp({
-      format: process.env.DATETIME_LOGGER_FORMAT || 'DD.MM.YYYY HH:mm:ss',
-    }),
-    format.json(),
-    format.printf((info) => {
-      return `${info.timestamp} ${info.level}: ${info.message}`;
-    }),
-  ),
 };
 
 export default createLogger(loggerOptions);
